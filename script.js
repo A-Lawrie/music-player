fd4ea1d68236fd046844543f2524316a62300a91
JCOCHARLIE <lawrieabuna@gmail.com>
2026-04-20 15:30:40 +0300

    album covers done, plus nilifanya a bit more styling

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const songs = [
   { title: "Booga", artist: "Central Cee", src: "audios/booga.mp3", img: "covers/booga.jpg" },
-  { title: "La Mudanza", artist: "Bad Bunny", src: "audios/la-mudanza.mp3" },
-  { title: "See you again", artist: "Wiz Khalifa", src: "audios/see-you-again.mp3" },
+  { title: "La Mudanza", artist: "Bad Bunny", src: "audios/la-mudanza.mp3", img: "covers/la-mudanza.jpg" },
+  { title: "See you again", artist: "Wiz Khalifa", src: "audios/see-you-again.mp3", img: "covers/see-you-again.jpg" },
 ];
 
 const playlist = document.getElementById("playlist");
 const player = document.getElementById("player");
+const cover = document.getElementById("cover");
+const title = document.getElementById("title");
 
 //dynamic loop
 songs.forEach((song, index) => {
@@ -19,4 +21,7 @@ function loadSong(index) {
   const song = songs[index];
   player.src = song.src;
   player.play();
+
+  cover.src = song.img;
+    title.textContent = `${song.title} — ${song.artist}`;
 }
