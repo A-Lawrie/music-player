15a9e1734d5eb7cd8b543feef0b9bef65b76ac76
JCOCHARLIE <lawrieabuna@gmail.com>
2026-04-20 15:45:33 +0300

    Managed to add duration but not how I wanted it to be(I'll look back into this and how to take it to the end)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const songs = [
-  { title: "Booga", artist: "Central Cee", src: "audios/booga.mp3", img: "covers/booga.jpg" },
-  { title: "La Mudanza", artist: "Bad Bunny", src: "audios/la-mudanza.mp3", img: "covers/la-mudanza.jpg" },
-  { title: "See you again", artist: "Wiz Khalifa", src: "audios/see-you-again.mp3", img: "covers/see-you-again.jpg" },
+  { title: "Booga", artist: "Central Cee", src: "audios/booga.mp3", img: "covers/booga.jpg", duration: "1:50" },
+  { title: "La Mudanza", artist: "Bad Bunny", src: "audios/la-mudanza.mp3", img: "covers/la-mudanza.jpg", duration: "3:35" },
+  { title: "See you again", artist: "Wiz Khalifa", src: "audios/see-you-again.mp3", img: "covers/see-you-again.jpg", duration: "3:00" },
 ];
 
 const playlist = document.getElementById("playlist");
@@ -12,7 +12,7 @@ const title = document.getElementById("title");
 //dynamic loop
 songs.forEach((song, index) => {
   const li = document.createElement("li");
-  li.textContent = `${song.title} — ${song.artist}`;
+  li.textContent = `${song.title} — ${song.artist} (${song.duration})`;
   li.addEventListener("click", () => loadSong(index));
   playlist.appendChild(li);
 });
